95c7e53248432f034e25aca531af1511745b65b5
memset0 <34177126+memset0@users.noreply.github.com>
2020-02-06 13:26:28 +0800

    fix #6.

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ sed -i "s/@SESSION_SECRET@/$SECRET/g" config/web.json
 sed -i "s/@JUDGE_TOKEN@/$SECRET/g" config/web.json
 sed -i "s/@JUDGE_TOKEN@/$SECRET/g" config/daemon.json
 sed -i "s/@EMAIL_JWT_SECRET@/$SECRET/g" config/web.json
-docker run -i --rm --network syzoj-docker_net mariadb:10.3 mysql -uroot -p123456 -hmysql < init.sql
+docker run -i --rm --network syzoj_net mariadb:10.3 mysql -uroot -p123456 -hmysql < init.sql
